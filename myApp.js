d4564aa34f5797feb4fc56a5b9eb162cf71ea11e
workerone1 <25370927-workerone1@users.noreply.replit.com>
2023-08-14 11:38:22 +0000

    fixes

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 let express = require('express');
 let app = express();
 let absolute_path = __dirname + "/views/index.html"
-let public_path = __dirname + "/public"
-
+app.use('/public', express.static(__dirname + "/public"));
 app.get('/', function(req, res) {
   res.sendFile(absolute_path)
 })
 
-app.use(express.static(public_path))
+
 
 
 
