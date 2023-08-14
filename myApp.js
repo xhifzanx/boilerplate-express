048ec024b31958799cd539d23202e6fcfd70be7e
workerone1 <25370927-workerone1@users.noreply.replit.com>
2023-08-14 11:32:40 +0000

    added static files

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let app = express();
+let absolute_path = __dirname + "/views/index.html"
+let public_path = __dirname + "/public"
 
 app.get('/', function(req, res) {
-  res.send('Hello Express')
+  res.sendFile(absolute_path)
 })
 
-
+app.use(express.static(public_path))
 
 
 
