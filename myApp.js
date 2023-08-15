ce427d6fa399b97ae4a3289756af8321af2413c0
workerone1 <25370927-workerone1@users.noreply.replit.com>
2023-08-15 05:46:50 +0000

    used dotenv functionality

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
-require('dotenv').config() 
+require('dotenv').config()
 var bodyParser = require("body-parser");
 let express = require('express');
 let app = express();
-bodyParser.urlencoded({extended: false})
+bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.json())
 
 
@@ -12,67 +12,37 @@ app.get('/', function(req, res) {
   res.sendFile(absolute_path)
 })
 
-app.use('/json',function(req, res, next) {
-	var string = req.method + " " + req.path + " - " + req.ip;
-	console.log(string)
-	next()
+app.use('/json', function(req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string)
+  next()
 })
 
 app.get('/json', function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
-	  response = "Hello json".toUpperCase();
-	} else {
-	  response = "Hello json";
-	}
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
   res.json({ "message": response })
 })
 
-app.get('/now', function(req, res, next){
-	req.time = new Date().toString()
-	next()
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString()
+  next()
 }, function(req, res) {
-	res.json({time: req.time})
-} )
+  res.json({ time: req.time })
+})
 
 app.get('/:word/echo', function(req, res) {
-	var word = req.params.word
-	res.json({echo: word})
+  var word = req.params.word
+  res.json({ echo: word })
 })
 
 app.get('/name', function(req, res) {
-	var name = req.query.first +' '+ req.query.last
-	res.json({"name": name})
+  var name = req.query.first + ' ' + req.query.last
+  res.json({ "name": name })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
